a04291f56367a2b458f3f65d316647a542eef1af
Justin Gu <justin.gu.1128@gmail.com>
2024-08-16 18:22:01 -0400

    MANA V1

--- src/main/java/org/me/lastpiece/Lastpiece.java
+++ src/main/java/org/me/lastpiece/Lastpiece.java
@@ -1,12 +1,28 @@
 package org.me.lastpiece;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.me.lastpiece.Events.Mana;
+import org.me.lastpiece.Events.playerJoin;
 
 public final class Lastpiece extends JavaPlugin {
+
+    private static Lastpiece plugin;
+    private static Mana manaInstance;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        plugin = this;
+
+        //Mana
+        manaInstance = new Mana();
+        manaInstance.init();
+        getServer().getPluginManager().registerEvents(new playerJoin(), this);
+
+
+        Bukkit.broadcastMessage(ChatColor.RED + "[SERVER] " + ChatColor.GREEN + "Lastpiece Plguin ");
 
     }
 
@@ -14,4 +30,13 @@ public final class Lastpiece extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public static Lastpiece getPlugin() {
+        return plugin;
+    }
+
+    public static Mana getManaInstance() {
+        return manaInstance;
+    }
+
 }
